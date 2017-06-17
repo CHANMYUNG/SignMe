908ee85f0450ca8410d6450dba2bcb979005088b
Terry Yun <nooheat1228@gmail.com>
2017-06-18 03:25:27 +0900

    Initail Commit : Basic Codes

---
+++ Server/SignMeServer/src/main/java/com/nooheat/manager/Manager.java
@@ -0,0 +1,10 @@
+package com.nooheat.manager;
+
+import io.vertx.ext.web.RoutingContext;
+import org.json.JSONObject;
+
+/**
+ * Created by NooHeat on 16/06/2017.
+ */
+public interface Manager {
+}
