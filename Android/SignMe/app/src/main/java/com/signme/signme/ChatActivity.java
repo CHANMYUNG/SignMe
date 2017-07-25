69e7f8e9474eb2667448d3df29081eee2e661b39
Hongsomang <ghdthakd12@naver.com>
2017-07-25 11:38:48 +0900

    채팅 레이아웃

---
+++ Android/SignMe/app/src/main/java/com/signme/signme/ChatActivity.java
@@ -0,0 +1,17 @@
+package com.signme.signme;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dsm2016 on 2017-07-21.
+ */
+
+public class ChatActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ Android/SignMe/app/src/main/java/com/signme/signme/ChatMessageAdapter.java
@@ -0,0 +1,56 @@
+package com.signme.signme;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by dsm2016 on 2017-07-20.
+ */
+
+public class ChatMessageAdapter extends ArrayAdapter {
+    List msgs=new ArrayList();
+    public ChatMessageAdapter(Context context,int textViewResouroeId){
+        super(context,textViewResouroeId);
+    }
+
+
+
+
+    //@Override
+    public void add(ChatMessage  object) {
+       msgs.add(object);
+        super.add(object);
+    }
+    @Override
+    public int getCount(){
+        return msgs.size();
+    }
+    @Override
+    public  ChatMessage getItem(int index){
+        return (ChatMessage) msgs.get(index);
+    }
+    @Override
+    public View getView(int position, View converView, ViewGroup parent){
+        View row=converView;
+        if(row==null){
+            LayoutInflater inflater=(LayoutInflater)this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row=inflater.inflate(R.layout.chatting_message,parent,false);
+        }
+        ChatMessage msg=(ChatMessage)msgs.get(position);
+        TextView msgText=(TextView)row.findViewById(R.id.chatmassage);
+        msgText.setText(msg.message);
+        msgText.setTextColor(Color.parseColor("#000000"));
+        return  row;
+    }
+
+
+}
