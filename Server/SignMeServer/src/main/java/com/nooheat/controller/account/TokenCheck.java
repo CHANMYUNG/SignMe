07e6971be8c6a0b46370fd47d19159ad44e52b5c
Terry Yun <nooheat1228@gmail.com>
2017-08-18 23:27:13 +0900

    [DONE] Token Check

--- Server/SignMeServer/src/main/java/com/nooheat/controller/account/TokenCheck.java
+++ Server/SignMeServer/src/main/java/com/nooheat/controller/account/TokenCheck.java
@@ -12,7 +12,7 @@ import io.vertx.ext.web.RoutingContext;
  * Created by NooHeat on 10/07/2017.
  */
 @API(category = Category.TEST, summary = "토큰 테스트", successCode = 200, failureCode = 401)
-@URIMapping(uri="/jwt", method = HttpMethod.POST)
+@URIMapping(uri="/jwt", method = HttpMethod.GET)
 public class TokenCheck implements Handler<RoutingContext> {
     @Override
     public void handle(RoutingContext context) {
