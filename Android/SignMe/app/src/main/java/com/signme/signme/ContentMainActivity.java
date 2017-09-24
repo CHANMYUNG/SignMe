1be4477f28522892c45d67c5c75b66934bcf2872
Hosomang <ghdthakd12@gmail.com>
2017-09-24 20:39:57 +0900

    2017.09.24

--- Android/SignMe/app/src/main/java/com/signme/signme/ContentMainActivity.java
+++ Android/SignMe/app/src/main/java/com/signme/signme/ContentMainActivity.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Canvas;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -14,7 +15,11 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import junit.framework.Test;
 
@@ -23,19 +28,25 @@ import junit.framework.Test;
  */
 
 public class ContentMainActivity extends AppCompatActivity {
-
+    private RadioGroup radioGroup;
+    private Button button;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content_main);
-
+        radioGroup=(RadioGroup)findViewById(R.id.radiogroup);
+        radioGroup.clearCheck();
+        String selectedResult="";
+        if(radioGroup.getCheckedRadioButtonId()>0){
+            View radioButton=radioGroup.findViewById(radioGroup.getCheckedRadioButtonId());
+            int radioid=radioGroup.indexOfChild((radioButton));
+            RadioButton btn=(RadioButton)radioGroup.getChildAt(radioid);
+            selectedResult=(String)btn.getText();
+        }
+        Toast.makeText(getApplicationContext(),selectedResult,Toast.LENGTH_SHORT).show();
     }
-    //가정통신문 제출 버튼
-    public void spend_contentonClick(View view){
-        //다이얼로그
 
-    }
     //사인 엑티비티 실행  but, 삭제 예정
     public void testonclick(View v) {
         Intent intent=new Intent(getApplicationContext(),TestDialog.class);
